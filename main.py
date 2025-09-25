e03db751119876c5d928433171f1c4b03d5b0797
Maverick Panda <skeletoncliquetoptyler@gmail.com>
2025-09-25 17:44:14 -0400

    Add 'quit' option

--- main.py
+++ main.py
@@ -35,46 +35,52 @@ special_characters = [
     ';',
     ':',
 ]
-mode = input("Would you like to encrypt or decrypt? ").strip().lower()
-key = input("Please enter your key (1-25): ").strip()
 
-if mode == "encrypt":
-    plaintext = input("Please enter your plaintext below:\n").lower()
-    encrypted_message = ""
-    for character in plaintext:
-        if character in special_characters:
-            encrypted_message += character
-        else:
-            encrypted_value = alphabet.get(character) + int(key)
-            # Catch wrapping values
-            if encrypted_value >= 26:
-                encrypted_value -= 26
-            elif encrypted_value <= -1:
-                encrypted_value += 26
-            # Convert encrypted_value to corresponding character
-            for k, v in alphabet.items():
-                if v == encrypted_value:
-                    encrypted_character = k
-                    break                
-            encrypted_message += encrypted_character
-    print(f"Your message has been encrypted:\n{encrypted_message}")
-elif mode == "decrypt":
-    ciphertext = input("Please enter your ciphertext below:\n").lower()
-    decrypted_message = ""
-    for character in ciphertext:
-        if character in special_characters:
-            decrypted_message += character
-        else:
-            decrypted_value = alphabet.get(character) - int(key)
-            # Catch wrapping values
-            if decrypted_value >= 26:
-                decrypted_value -= 26
-            elif decrypted_value <= -1:
-                decrypted_value += 26
-            # Convert decrypted_value to corresponding character
-            for k, v in alphabet.items():
-                if v == decrypted_value:
-                    decrypted_character = k
-                    break
-            decrypted_message += decrypted_character
-    print(f"Your message has been decrypted:\n{decrypted_message}") 
+active = True
+while active:
+    mode = input("\nWould you like to encrypt or decrypt?\n").strip().lower()
+    key = input("Please enter your key (1-25): ").strip()
+    if mode == "encrypt":
+        plaintext = input("Please enter your plaintext below:\n").lower()
+        encrypted_message = ""
+        for character in plaintext:
+            if character in special_characters:
+                encrypted_message += character
+            else:
+                encrypted_value = alphabet.get(character) + int(key)
+                # Catch wrapping values
+                if encrypted_value >= 26:
+                    encrypted_value -= 26
+                elif encrypted_value <= -1:
+                    encrypted_value += 26
+                # Convert encrypted_value to corresponding character
+                for k, v in alphabet.items():
+                    if v == encrypted_value:
+                        encrypted_character = k
+                        break                
+                encrypted_message += encrypted_character
+        print(f"\nYour message has been encrypted:\n{encrypted_message}")
+    elif mode == "decrypt":
+        ciphertext = input("Please enter your ciphertext below:\n").lower()
+        decrypted_message = ""
+        for character in ciphertext:
+            if character in special_characters:
+                decrypted_message += character
+            else:
+                decrypted_value = alphabet.get(character) - int(key)
+                # Catch wrapping values
+                if decrypted_value >= 26:
+                    decrypted_value -= 26
+                elif decrypted_value <= -1:
+                    decrypted_value += 26
+                # Convert decrypted_value to corresponding character
+                for k, v in alphabet.items():
+                    if v == decrypted_value:
+                        decrypted_character = k
+                        break
+                decrypted_message += decrypted_character
+        print(f"\nYour message has been decrypted:\n{decrypted_message}")
+    
+    again = input("Continue (y/n)? ").strip().lower()
+    if again == 'n':
+        active = False
